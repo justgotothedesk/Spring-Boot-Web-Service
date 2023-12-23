536f6f930d48ef19baaf7eb361c796b9648de2bf
JinCheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2023-12-23 21:38:14 +0900

    Update Day1.java

--- src/main/java/com/example/demo/Day1.java
+++ src/main/java/com/example/demo/Day1.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -58,5 +60,15 @@ class RestApiDemoController{
     @GetMapping("/coffees")
     Iterable<Coffee> getCoffees(){
         return coffees;
+    }
+    
+    @GetMapping("/coffees/{id}")
+    Optional<Coffee> getCoffeeById(@PathVariable String id){
+        for(Coffee c: coffees){
+            if (c.getId().equals(id)){
+                return Optional.of(c);
+            }
+        }
+        return Optional.empty();
     }
 }
