de83b9e4168ad771898cc47fc99cda03df6a6d24
JinCheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2023-12-23 21:42:05 +0900

    Update Day1.java

--- src/main/java/com/example/demo/Day1.java
+++ src/main/java/com/example/demo/Day1.java
@@ -2,9 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -61,7 +59,7 @@ class RestApiDemoController{
     Iterable<Coffee> getCoffees(){
         return coffees;
     }
-    
+
     @GetMapping("/coffees/{id}")
     Optional<Coffee> getCoffeeById(@PathVariable String id){
         for(Coffee c: coffees){
@@ -70,5 +68,29 @@ class RestApiDemoController{
             }
         }
         return Optional.empty();
+    }
+    
+    @PostMapping("/coffees")
+    Coffee postCoffee(@RequestBody Coffee coffee){
+        coffees.add(coffee);
+        return coffee;
+    }
+    
+    @PutMapping("/coffees/{id}")
+    Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee){
+        int index = -1;
+        
+        for(Coffee c: coffees){
+            if(c.getId().equals(id)){
+                index = coffees.indexOf(c);
+                coffees.set(index, coffee);
+            }
+        }
+        return (index == -1)?postCoffee(coffee):coffee;
+    }
+    
+    @DeleteMapping("/coffees/{id}")
+    void deleteCoffee(@PathVariable String id){
+        coffees.removeIf(c -> c.getId().equals(id));
     }
 }
