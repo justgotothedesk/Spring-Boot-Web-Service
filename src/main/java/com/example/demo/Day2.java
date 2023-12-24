bede30e65a513d8a469303711969723a2b554eae
Shin <justgotothedesk@naver.com>
2023-12-25 00:22:41 +0900

    Day3 practice

--- src/main/java/com/example/demo/Day2.java
+++ src/main/java/com/example/demo/Day2.java
@@ -1,4 +1,3 @@
-// Day 2 : JPA Entity 설정 및 Day 1에서의 CRUD 매핑 기능 간단하게 줄이기
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
@@ -18,83 +17,5 @@ import java.util.UUID;
 public class Day2 {
     public static void main(String[] args) {
         SpringApplication.run(Day2.class, args);
-    }
-}
-
-interface CoffeeRepository extends CrudRepository<Coffee2, String>{}
-
-@Entity
-class Coffee2 {
-    @Id
-    private final String id;
-    private String name;
-
-    public Coffee2(String id, String name){
-        this.id = id;
-        this.name = name;
-    }
-
-    public Coffee2() {
-        this.id = UUID.randomUUID().toString();
-    }
-
-    public Coffee2(String name){
-        this(); // 기본 생성자 호출
-        this.name = name;
-    }
-    public String getId(){
-        return id;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-}
-
-@RestController
-@RequestMapping("/coffees2")
-class RestApiDemoController2{
-    private final CoffeeRepository coffeeRepository;
-
-    public RestApiDemoController2(CoffeeRepository coffeeRepository){
-        this.coffeeRepository = coffeeRepository;
-
-        this.coffeeRepository.saveAll(List.of(
-                new Coffee2("Cafe Cereza"),
-                new Coffee2("Cafe Ganador"),
-                new Coffee2("Cafe Lareno"),
-                new Coffee2("Cafe Tres Pontas")
-        ));
-    }
-
-    @GetMapping
-    Iterable<Coffee2> getCoffees(){
-        return coffeeRepository.findAll();
-    }
-
-    @GetMapping("/{id}")
-    Optional<Coffee2> getCoffeeById(@PathVariable String id){
-        return coffeeRepository.findById(id);
-    }
-
-    @PostMapping
-    Coffee2 postCoffee(@RequestBody Coffee2 coffee){
-        return coffeeRepository.save(coffee);
-    }
-
-    @PutMapping("/{id}")
-    ResponseEntity<Coffee2> putCoffee(@PathVariable String id, @RequestBody Coffee2 coffee){
-        return (!coffeeRepository.existsById(id))?
-                new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED):
-                new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{id}")
-    void deleteCoffee(@PathVariable String id){
-        coffeeRepository.deleteById(id);
     }
 }
