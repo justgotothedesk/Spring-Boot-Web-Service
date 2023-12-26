bbb912b3d19ff05319138ef7ccd1db6305234104
JinCheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2023-12-26 14:42:42 +0900

    Update Day3.java

--- src/main/java/com/example/demo/Day3.java
+++ src/main/java/com/example/demo/Day3.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Day3 {
     public static void main(String[] args) {
         SpringApplication.run(Day3.class, args);
@@ -17,20 +20,41 @@ public class Day3 {
 @RestController
 @RequestMapping("/greeting")
 class GreetingController{
-    @Value("${greeting-name : Mirage}")
-    private String name;
+    private final Greeting greeting;
 
-    @Value("${greeting-coffee : ${greeting-name} is drinking Cafe Ganador}")
-    private String coffee;
+    public GreetingController(Greeting greeting) {
+        this.greeting = greeting;
+    }
 
     @GetMapping
     String getGreeting(){
-        return name;
+        return greeting.getName();
     }
 
     @GetMapping("/coffee")
     String getNameCoffee(){
-        return coffee;
+        return greeting.getCoffee();
     }
 }
 
+@ConfigurationProperties(prefix = "greeting")
+class Greeting{
+    private String name;
+    private String coffee;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getCoffee(){
+        return coffee;
+    }
+
+    public void setCoffee(String coffee){
+        this.coffee = coffee;
+    }
+}
