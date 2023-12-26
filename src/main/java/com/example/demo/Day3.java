ddd6ea41fd52d99c176174052852048aac641c0e
JinCheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2023-12-26 14:46:55 +0900

    Update Day3.java

--- src/main/java/com/example/demo/Day3.java
+++ src/main/java/com/example/demo/Day3.java
@@ -1,3 +1,5 @@
+// Day3 : configuration 속성 사용 및 Actuator 사용법 익히기
+
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -5,15 +7,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.ranges.DocumentRange;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class Day3 {
     public static void main(String[] args) {
         SpringApplication.run(Day3.class, args);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "droid")
+    Droid createDroid(){
+        return new Droid();
     }
 }
 
@@ -56,5 +66,40 @@ class Greeting{
 
     public void setCoffee(String coffee){
         this.coffee = coffee;
+    }
+}
+
+class Droid{
+    private String id, description;
+
+    public String getId(){
+        return id;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+}
+
+@RestController
+@RequestMapping("/droid")
+class DroidController{
+    private final Droid droid;
+
+    public DroidController(Droid droid){
+        this.droid = droid;
+    }
+
+    @GetMapping
+    Droid getDroid(){
+        return droid;
     }
 }
