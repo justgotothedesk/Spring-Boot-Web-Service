958892247c8a137b42242c0c0f7b2ee56d2f42a0
JinCheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2023-12-29 15:11:58 +0900

    Update PositionController.java

--- src/main/java/com/example/demo/PositionController.java
+++ src/main/java/com/example/demo/PositionController.java
@@ -1,25 +1,46 @@
 package com.example.demo;
 
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 @RequiredArgsConstructor
 @Controller
 public class PositionController {
-    @NonNull
     private final AircraftRepository repository;
+    private final RSocketRequester requester;
+    private WebClient client = WebClient.create("http://localhost:7634/aircraft");
 
-    public PositionController(AircraftRepository repository) {
+    public PositionController(AircraftRepository repository, RSocketRequester requester) {
         this.repository = repository;
+        this.requester = RSocketRequester.builder().tcp("localhost", 7635);
     }
 
     @GetMapping("/aircraft")
     public String getCurrentAircraftPositions(Model model) {
-        model.addAttribute("currentPositions", repository.findAll());
+        Flux<Aircraft> aircraftFlux = repository.deleteAll()
+                        .thenMany(client.get()
+                                .retrieve()
+                                .bodyToFlux(Aircraft.class)
+                                .filter(plane -> !plane.getReg().isEmpty())
+                                .flatMap(repository::save));
+
+        model.addAttribute("currentPositions", aircraftFlux);
         return "positions";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/acstream",
+               produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Aircraft> getCurrentACPositionsStream() {
+        return requester.route("acstream")
+                .data("Requesting aircraft positions")
+                .retrieveFlux(Aircraft.class);
     }
 }
