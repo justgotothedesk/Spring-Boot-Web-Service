d120341ae2a844cddf0e98aa546213e7f8c6d0e1
Shin <justgotothedesk@naver.com>
2023-12-30 16:50:47 +0900

    Day7 practice

---
+++ src/test/java/com/example/demo/PositionControllerTest.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.Instant;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebFluxTest({PositionControllerTest.class})
+public class PositionControllerTest {
+
+    private Aircraft ac1, ac2;
+
+    @MockBean
+    private PositionRetriever retriever;
+
+    @MockBean
+    private AircraftRepository repository;
+
+    @BeforeEach
+    void setUp(ApplicationContext context) {
+        ac1 = new Aircraft(1L, "SAL001", "sqwk", "N12345", "SAL001",
+                "STL-SF0", "LJ", "ct",
+                30000, 280, 440, 0, 0,
+                39.2979849, -94.71921, 0D, 0D, 0D,
+                true, false,
+                Instant.now(), Instant.now(), Instant.now());
+
+        ac2 = new Aircraft(1L, "SAL001", "sqwk", "N12345", "SAL001",
+                "STL-SF0", "LJ", "ct",
+                30000, 280, 440, 0, 0,
+                39.2979849, -94.71921, 0D, 0D, 0D,
+                true, false,
+                Instant.now(), Instant.now(), Instant.now());
+
+        Mockito.when(retriever.retrieveAircraftPositions()
+                .thenReturn(List.of(ac1, ac2)));
+    }
+    @AfterEach
+    void tearDown() {
+
+    }
+    @Test
+    void getCurrentAircraftPositions(@Autowired WebTestClient client) {
+        assert client.get()
+                .uri("/aircraft")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Iterable.class)
+                .returnResult()
+                .getResponseBody()
+                .iterator()
+                .hasNext();
+    }
+}
