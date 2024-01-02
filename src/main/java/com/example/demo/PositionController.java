c99c8e8bd0bab4aa0b72877dc4f5187c709ab5a6
justgotothedesk <wlscjf3877@naver.com>
2024-01-02 15:30:22 +0900

    Day10 practice

--- src/main/java/com/example/demo/PositionController.java
+++ src/main/java/com/example/demo/PositionController.java
@@ -1,60 +1,36 @@
 package com.example.demo;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-//@RequiredArgsConstructor
-//@Controller
-//public class PositionController {
-//    private final AircraftRepository repository;
-//    private final RSocketRequester requester;
-//    private WebClient client = WebClient.create("http://localhost:7634/aircraft");
-//
-//    public PositionController(AircraftRepository repository, RSocketRequester requester) {
-//        this.repository = repository;
-//        this.requester = RSocketRequester.builder().tcp("localhost", 7635);
-//    }
-//
-//    @GetMapping("/aircraft")
-//    public String getCurrentAircraftPositions(Model model) {
-//        Flux<Aircraft> aircraftFlux = repository.deleteAll()
-//                        .thenMany(client.get()
-//                                .retrieve()
-//                                .bodyToFlux(Aircraft.class)
-//                                .filter(plane -> !plane.getReg().isEmpty())
-//                                .flatMap(repository::save));
-//
-//        model.addAttribute("currentPositions", aircraftFlux);
-//        return "positions";
-//    }
-//
-//    @ResponseBody
-//    @GetMapping(value = "/acstream",
-//               produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-//    public Flux<Aircraft> getCurrentACPositionsStream() {
-//        return requester.route("acstream")
-//                .data("Requesting aircraft positions")
-//                .retrieveFlux(Aircraft.class);
-//    }
-//}
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @AllArgsConstructor
 @RestController
 public class PositionController {
-    private final PositionRetriever retriever;
+    private final PositionService service;
+    private final RSocketRequester requester;
 
-    public PositionController(PositionRetriever retriever) {
-        this.retriever = retriever;
+    public PositionController(PositionService service, RSocketRequester requester) {
+        this.service = service;
+        this.requester = requester;
     }
 
     @GetMapping("/aircraft")
-    public Iterable<Aircraft> getCurrentAircraftPositions() {
-        return retriever.retrieveAircraftPositions();
+    public String getCurrentAircraftPositions(Model model) {
+        model.addAttribute("currentPositions", service.getAllAircraft());
+
+        return "positions";
     }
 
-    @GetMapping("/aircraftadmin")
-    public Iterable<Aircraft> getCurrentAircraftPositionsAdminPrivs() {
-        return retriever.retrieveAircraftPositions();
+    @ResponseBody
+    @GetMapping(value = "/acstream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Aircraft> getCurrentACPositionsStream() {
+        return requester.route("acstream").data("Requesting aircraft positions").retrieveFlux(Aircraft.class);
     }
 }
